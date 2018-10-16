ba798e96114eee8ab77326985d0048e491a96b24
DrewSK <drew.koballa@gmail.com>
2018-10-16 15:18:47 -0500

    sends to PTP and opens browser

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 import * as vscode from 'vscode';
 import * as request from 'request-promise-native';
 import * as sqlops from 'sqlops';
+import { open } from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -17,23 +18,33 @@ export function activate(context: vscode.ExtensionContext) {
 
     var pasteThePlan = async () => {
         vscode.window.showInformationMessage('starting the ptp');
-        var planXML = vscode.window.activeTextEditor.document.getText();
-        vscode.window.showInformationMessage('GOT THE XML');
-        try {
-                var options = {
-                uri: 'https://jeczi7iqj8.execute-api.us-west-2.amazonaws.com/prod/',
-                body: {"queryplan_xml": planXML},
-                json: true
-            };
-            const pastedPlan = await request.post(options); 
-            vscode.window.showInformationMessage('called PTP');      
-            vscode.window.showInformationMessage(pastedPlan.id)
-        } catch (err) {
-            vscode.window.showErrorMessage(err);
+        if (vscode.window.activeTextEditor.document.languageId === 'xml') {
+            var planXML = vscode.window.activeTextEditor.document.getText();
+            vscode.window.showInformationMessage('GOT THE XML');
+            sendToPTP(planXML);
+        } else {
+            vscode.window.showInformationMessage('sorry, Paste the Plan connector is supported for XML files only');
         }
     };
     var disposable_ptp = vscode.commands.registerCommand('extension.pastePlan', pasteThePlan);
     context.subscriptions.push(disposable_ptp);
+
+    async function sendToPTP(planXML){
+        try {
+            var options = {
+            uri: 'https://jeczi7iqj8.execute-api.us-west-2.amazonaws.com/prod/',
+            body: {"queryplan_xml": planXML},
+            json: true
+        };
+        const pastedPlan = await request.post(options); 
+        vscode.window.showInformationMessage('called PTP');      
+        vscode.window.showInformationMessage(pastedPlan.id);
+        var newPTPlink = 'https://www.brentozar.com/pastetheplan/?id='+pastedPlan.id;
+        vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(newPTPlink));
+        } catch (err) {
+            vscode.window.showErrorMessage(err);
+        }
+    }
 }
 
 // this method is called when your extension is deactivated
